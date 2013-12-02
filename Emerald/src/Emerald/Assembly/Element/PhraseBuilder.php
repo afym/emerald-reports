0dd9faeaa3cea42f950a9b8aa22d9094d8e7cdff
afym <a_fym@hotmail.com>
2013-12-02 12:21:13 -0500

    bottom and left.

--- Emerald/src/Emerald/Assembly/Element/PhraseBuilder.php
+++ Emerald/src/Emerald/Assembly/Element/PhraseBuilder.php
@@ -12,13 +12,15 @@ class PhraseBuilder extends ElementBuilder
 
     private $phrase;
     private $format;
+    private $fontReference;
     private $text;
 
-    public function __construct(Phrase $phrase, Format $format)
+    public function __construct(Phrase $phrase, Format $format, $fontReference)
     {
         parent::__construct();
         $this->phrase = $phrase;
         $this->format = $format;
+        $this->fontReference = $fontReference;
         $this->text = new Text();
     }
 
@@ -30,7 +32,33 @@ class PhraseBuilder extends ElementBuilder
     private function buildText()
     {
         $this->text->setValue(array(
-            
+            'f' => $this->fontReference, 
+            's' => $this->phrase->getSize(), 
+            'l' => $this->getLeft(), 
+            'b' => $this->getBottom(), 
+            't' => $this->phrase->getString(),
         ));
+    }
+    
+    private function getBottom()
+    {
+        $height = $this->format->getHeight() - (2 * $this->format->getBottom());
+
+        if ($this->phrase->getBottom() < $height)
+            return $this->phrase->getBottom() + $this->format->getBottom();
+        }
+
+        return $height;
+    }
+    
+    private function getLeft()
+    {
+        $width = $this->format->getWidth() - (2 * $this->format->getLeft());
+
+        if ($this->phrase->getLeft() < $width)
+            return $this->phrase->getLeft() + $this->format->getLeft();
+        }
+
+        return $width;
     }
 }
