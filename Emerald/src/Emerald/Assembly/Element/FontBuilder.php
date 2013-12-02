ee116aa2b6d7e7762e60831e023a92e88a22909c
afym <a_fym@hotmail.com>
2013-12-02 11:06:34 -0500

    Font builder added.

--- Emerald/src/Emerald/Assembly/Element/FontBuilder.php
+++ Emerald/src/Emerald/Assembly/Element/FontBuilder.php
@@ -3,9 +3,34 @@
 namespace Emerald\Assembly\Element;
 
 use Emerald\Assembly\Document\Abstracts\ElementBuilder;
+use Emerald\Type\FontType;
+use Emerald\Pdf\Page\Font;
 
 class FontBuilder extends ElementBuilder
 {
+    private $fontType;
+    private $font;
 
- 
+    public function __construct(Font $font)
+    {
+        $this->fontType = new FontType();
+        $this->font = $font;
+    }
+
+    public function build()
+    {
+        $this->buildFontType();
+        return $this->out;
+    }
+
+    private function buildFontType()
+    {
+        $this->fontType->setValue(array(
+             'f' => $this->font->getFamily(), 
+             'b' => $this->font->getBold(), 
+             'e' => $this->font->getEncoding(),
+        ));
+        
+        $this->append($this->fontType->out());
+    }
 }
