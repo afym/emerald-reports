d5affbc018a1e23f26f30e27bedd8eaa0441b128
afym <a_fym@hotmail.com>
2013-12-03 11:28:46 -0500

    modified

--- Emerald/src/Emerald/Assembly/Document/TrailerBuilder.php
+++ Emerald/src/Emerald/Assembly/Document/TrailerBuilder.php
@@ -7,6 +7,14 @@ use Emerald\Assembly\Document\Abstracts\Builder;
 
 class TrailerBuilder extends Builder
 {
+    private $trailerStack;
+
+    public function __construct(TrailerStack $trailerStack)
+    {
+        parent::__construct();
+        $this->trailerStack = $trailerStack;        
+    }
+
     public function build()
     {
       return $this->out;
