f7211769f2005014313d44400b20463d9f107be3
afym <a_fym@hotmail.com>
2013-12-03 11:05:41 -0500

    Methods added

--- Emerald/src/Emerald/Assembly/Document/TrailerStack.php
+++ Emerald/src/Emerald/Assembly/Document/TrailerStack.php
@@ -3,14 +3,26 @@
 namespace Emerald\Assembly\Document;
 
 use Emerald\Assembly\Document\Abstracts\Stack;
+use Emerald\Document\Object;
 
 class TrailerStack extends Stack
 {
-
     
-    public function make()
+    private $objects;
+    
+    public function __construct()
     {
-        
+        $this->objects = new \ArrayObject();
     }
 
+    public function appendObject(Object $object)
+    {
+        $this->objects->append($object);
+        return $this;
+    }
+
+    public function getObjects()
+    {
+        return $this->objects;
+    }
 }
