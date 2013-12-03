3b1f370b56b7bdcf52c074e18e590fb283bb1367
afym <a_fym@hotmail.com>
2013-12-03 17:16:15 -0500

    Fonts builder

--- Emerald/src/Emerald/Assembly/Resource/ResourceBuilder.php
+++ Emerald/src/Emerald/Assembly/Resource/ResourceBuilder.php
@@ -3,31 +3,56 @@
 namespace Emerald\Assembly\Resource;
 
 use Emerald\Assembly\Document\Abstracts\ElementBuilder;
+use Emerald\Assembly\Resource\FontBuilder;
+use Emerald\Assembly\Resource\ResourceStack;
+use Emerald\Pdf\Page\Font;
 use Emerald\Type\Resources;
 
-class FontBuilder extends ElementBuilder
+class ResourceBuilder extends ElementBuilder
 {
-
+    private $resourceStack;
     private $resourcesType;
+    private $xobjectsOut;
+    private $fontsOut;
     private $font;
 
-    public function __construct()
+    public function __construct(ResourceStack $resourceStack)
     {
         $this->resourcesType = new Resources();
+        $this->resourceStack  = resourceStack;
+        $this->fontsOut = '';
+        $this->xobjectsOut = '';
     }
 
     public function build()
     {
-       
+        $this->buildFonts();
+        $this->buildResourceType();
+
         return $this->out;
     }
 
-    private function buildFontType()
+    private function buildResourceType()
     {
         $this->resourcesType->setValue(array(
-            'f' => '',
-            'x' => '',
+            'f' => $this->fontsOut,
+            'x' => $this->xobjectsOut,
         ));
+        
+        $this->append($this->resourcesType->out());
     }
 
+    private function buildFonts()
+    {
+        $fonts = $this->resourceStack->getFonts();
+
+        foreach ($fonts as $reference => $font) {
+            $this->fontsOut .= $this->buildFont($reference, $font);
+        }
+    }
+
+    private function buildFont($reference, Font $font)
+    {
+        return (new FontBuilder($reference, $font))->buld();
+    }
 }
