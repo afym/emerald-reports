626b4dc6fa0d57f87fb6cc1383694d5583a6b88f
afym <a_fym@hotmail.com>
2013-12-03 17:07:58 -0500

    Reference added.

--- Emerald/src/Emerald/Type/FontType.php
+++ Emerald/src/Emerald/Type/FontType.php
@@ -18,11 +18,11 @@ class FontType extends Type
     public function __construct()
     {
         parent::__construct();
-        $this->format = '<</Type /Font /BaseFont /%s /Subtype /Type1 /Encoding /%s >>';
+        $this->format = '<< /%s <</Type /Font /BaseFont /%s /Subtype /Type1 /Encoding /%s >> >>';
     }
 
     /**
-     * Set font's name , bold  and endcoding (f, b, e)
+     * Set font's reference, name , bold  and endcoding (r, f, b, e)
      * 
      * @param Array $values
      *
@@ -35,7 +35,7 @@ class FontType extends Type
         }
         // -Oblique
 
-        $this->out = sprintf($this->format, $value['f'], $value['e']);
+        $this->out = sprintf($this->format, $value['r'], $value['f'], $value['e']);
     }
 
 }
