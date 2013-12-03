0cdfefebd7c7c86dea14575a455ced34bb6af782
afym <a_fym@hotmail.com>
2013-12-03 11:31:34 -0500

    setValue method added.

--- Emerald/src/Emerald/Type/TrailerSize.php
+++ Emerald/src/Emerald/Type/TrailerSize.php
@@ -1,6 +1,29 @@
 <?php
 
-class TrailerSize
+
+namespace Emerald\Type;
+
+use Emerald\Type\Abstracts\Type;
+
+
+class TrailerSize extends Type
 {
-  // << /Size %s /Root %s /Info %s >>
+    public function __construct()
+    {
+        parent::__construct();
+        $this->format = '<< /Size %s /Root %s /Info %s >>';
+    }
+
+    /**
+     * Set size, root and info (s, r, i)
+     * 
+     * @param Array $values
+     *
+     * @return void
+     */
+    public function setValue($value)
+    {
+        $this->out = sprintf($this->format, $value['s'], $value['r'], $value['i']);
+    }
+
 }
